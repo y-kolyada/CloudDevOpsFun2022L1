e7f68c8174d8784eda6591e868e4f2e814bd04d3
Yakov Kolyada <yakov.kolyada@gmail.com>
2022-11-28 22:34:29 +0200

    LinuxNetworking delete old

--- 02EDOL1LinuxNetworking/0203EDOL1003StaticRouting/0203EDOL1003StaticRoutingMicrotikGWRouting.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-export  file=add_cl1-cl2_network
-# 10.0.10.2 - host server
-add distance=1 dst-address=10.3.72.0/24 gateway=10.0.10.2
-add distance=1 dst-address=10.72.15.0/24 gateway=10.0.10.2
-add action=masquerade chain=srcnat comment="default configuration" out-interface=ether1-WAN-DG
