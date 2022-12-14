814a700e6673929659645756e60f36cb0c14e983
Yakov Kolyada <yakov.kolyada@gmail.com>
2022-12-14 16:52:16 +0200

    Task 0702 Terrafrom Lab4 Destroy Infra v0.0.1

---
+++ 07EDOL1Terraform/07EDOL1003TerraformAWSLab/07EDOL1003Terraform_AWSLab5_DestroyInfrastructure.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+# Prerequisites
+#
+# https://developer.hashicorp.com/terraform/tutorials/aws-get-started/aws-change
+
+
+# export AWS_ACCESS_KEY_ID= ...
+# export AWS_SECRET_ACCESS_KEY= ...
+
+terraform destroy
+
