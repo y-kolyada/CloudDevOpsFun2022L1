619697de7e4ec65af1a4c914d3431e9a60565af8
Yakov Kolyada <yakov.kolyada@gmail.com>
2022-12-22 21:27:52 +0200

    0104 Bash Java DemoMaven App CLI v0.0.1

---
+++ 01EDOL1LinuxBash/java/DemoMaven/demo/src/main/java/com/ygorod/App.java
@@ -0,0 +1,13 @@
+package com.ygorod;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
